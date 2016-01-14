5ce2d04d340025145d049c7d113da5308bd724bd
mail@coskuntekin.com <mail@coskuntekin.com>
2016-01-14 10:58:01 +0800

    fix loading issue

--- app/app.js
+++ app/app.js
@@ -3,25 +3,28 @@
 
 // Use new ES6 modules syntax for everything.
 import os from 'os'; // native node.js module
-import { remote } from 'electron'; // native electron module
+import {
+    remote
+}
+from 'electron'; // native electron module
 import jetpack from 'fs-jetpack'; // module loaded from npm
-import { greet } from './hello_world/hello_world'; // code authored by you in this project
+import {
+    greet
+}
+from './hello_world/hello_world'; // code authored by you in this project
 import env from './env';
 
 var app = remote.app;
 var appDir = jetpack.cwd(app.getAppPath());
 
 onload = function() {
-    var webview = document.getElementById("webview-bulutfon");
-    var webviewFrame = document.querySelector(".js-webview-frame");
-    var webviewLoading = document.querySelector(".js-webview-loading");
+    var webview = document.getElementById("js-webview-bulutfon");
+    var loading = document.querySelector(".js-webview-loading");
 
-    var loadstart = function() {
-    }
+    var loadstart = function() {};
     var loadstop = function() {
-        webviewFrame.classList.remove('hide');
-        webviewLoading.classList.add('hide');
-    }
+        loading.classList.add('hide');
+    };
     webview.addEventListener("did-start-loading", loadstart);
     webview.addEventListener("did-stop-loading", loadstop);
-}
+};
